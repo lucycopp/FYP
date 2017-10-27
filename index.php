7f9397707cdd39959e36bf44670ac4532cb1c966
lucycopp <lucyanncopp@hotmail.co.uk>
2017-10-27 23:23:03 +0100

    Attempt at adding function

--- index.php
+++ index.php
@@ -14,16 +14,25 @@ if ($conn === false)
     die(print_r(sqlsrv_errors() , true));
 }
 else {
-    echo "Connected";
+    echo "Connected - Trying to add";
 }
-$sql = "INSERT INTO UserTable (Username, Guide?)
+$sql = "INSERT INTO UserTable('Username', 'Guide?')
 VALUES ('Lucy', '1')";
-
+$conn -> query($sql);
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+
+
+
+
+
+
+
+
 function addToUserTable($username, $guide, $conn){
     $sql = "INSERT INTO UserTable('Username', 'Guide?') VALUES ('WILL', 1)";
     $conn -> query($sql);
