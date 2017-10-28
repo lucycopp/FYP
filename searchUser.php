2e27ebad821ec58ec506c7660167eec4070612db
lucycopp <lucyanncopp@hotmail.co.uk>
2017-10-29 00:52:10 +0100

    Added search User

--- searchUser.php
+++ searchUser.php
@@ -6,9 +6,18 @@
  * Time: 23:38
  */
 require 'index.php';
-echo $_GET['name'];
+$username = $_GET['name'];
 
+$sql = "SELECT * FROM UserTable WHERE Username=(?) FOR JSON AUTO";
+$parameters =  array($username);
+$stmt = sqlsrv_query($connection, $sql, $parameters);
+if ($stmt) {
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_NUMERIC)) {
+        echo $row[0];
+    }
+    sqlsrv_free_stmt($stmt);
+}
+else {die(print_r(sqlsrv_errors(), true)); }
 
-searchForRecordInUserTableUsernameReturnJSON($_GET['name'], $conn);
 
 ?>
