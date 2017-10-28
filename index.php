58246b0e63f69807b488ffc63b6dd95056ad63b3
lucycopp <lucyanncopp@hotmail.co.uk>
2017-10-28 21:37:30 +0100

    Working search not WHERE

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ function addToUserTable($username, $guide, $conn){
 }
 
 function searchForRecordInUserTableUsername ($username, $connection){
-    $sql ="SELECT * FROM UserTable WHERE Username='Lucy'";
+    $sql ="SELECT ID FROM UserTable WHERE Username='Lucy'";
    // $params = 'Lucy';
     $stmt = sqlsrv_query($connection, $sql);
     if($stmt) {
