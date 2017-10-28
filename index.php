c1c88e7d5c0393f30348070faa574ebb473e3d19
lucycopp <lucyanncopp@hotmail.co.uk>
2017-10-28 21:47:08 +0100

    Working search not WHERE

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $connectionOptions = array(
     "PWD" => "Bubble2017"
 );
 $conn = sqlsrv_connect($serverName, $connectionOptions);
-searchForRecordInUserTableUsername("WILL", $conn);
+searchForRecordInUserTableUsername('WILL', $conn);
 
 function addToUserTable($username, $guide, $conn){
 
