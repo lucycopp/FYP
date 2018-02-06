228be420b85f4b7edc5fc757f3ea90c24a453639
lucycopp <lucyanncopp@hotmail.co.uk>
2018-02-06 15:04:11 +0000

    Added functionality to add

---
+++ addAccessPoint.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucy
+ * Date: 06/02/2018
+ * Time: 14:58
+ */
+
+require 'index.php';
+
+$roomID = $_GET['ID'];
+$MAC = $_GET['mac'];
+$RSSI = $_GET['rssi'];
+
+
+
+
+$sql =  "INSERT INTO AccessPointsTavle('Location ID', RSSI, MAC) VALUES (?, ?, ?)";
+$params = array($roomID, $RSSI, $MAC);
+
+$stmt = sqlsrv_query( $conn, $sql, $params);
+if( $stmt === false ) {
+    die( print_r( sqlsrv_errors(), true));
+}else {echo "Added"; }
+
+
+
+?>
