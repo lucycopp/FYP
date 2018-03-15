7043252e6af91200e2a614e955f9c9932b20cb13
lucycopp <lucyanncopp@hotmail.co.uk>
2018-03-15 19:06:46 +0000

    RoomTable should now be updating correctly

--- addRoom.php
+++ addRoom.php
@@ -8,7 +8,18 @@
 
 require 'index.php';
 
-echo "GOT ROOM";
+$Name = $_GET['Name'];
+$Time = $_GET['Time'];
+
+
+$sql = "INSERT INTO RoomTable(Name, Time) VALUES (?, ?)";
+$params = array($Name, $Time);
+
+$stmt = sqlsrv_query($conn, $sql, $params);
+if( $stmt === false ) {
+    die( print_r( sqlsrv_errors(), true));
+    echo "Not added";
+}else {echo "Room Added"; }
 
 
 ?>
