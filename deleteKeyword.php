1874b09031623d60a24c5f12998acde63aefbb9b
lucycopp <lucyanncopp@hotmail.co.uk>
2018-03-26 21:13:17 +0100

    Added functionality to delete keywords

--- deleteKeyword.php
+++ deleteKeyword.php
@@ -3,24 +3,24 @@
  * Created by PhpStorm.
  * User: Lucy
  * Date: 26/03/2018
- * Time: 12:25
+ * Time: 21:10
  */
 
 require 'index.php';
 
-$roomID = $_GET['ID'];
-$keyword = $_GET['Keyword'];
+$ID = $_GET['ID'];
 
 
 
 
-$sql =  "INSERT INTO KeywordsTable(Keyword, \"Location ID\") VALUES (?, ?)";
-$params = array($keyword, $roomID);
+
+$sql =  "DELETE * FROM KeywordsTable WHERE ID = (?) VALUES (?)";
+$params = array($ID);
 
 $stmt = sqlsrv_query( $conn, $sql, $params);
 if( $stmt === false ) {
     die( print_r( sqlsrv_errors(), true));
-}else {echo "Added"; }
+}else {echo "Deleted"; }
 
 
 ?>
