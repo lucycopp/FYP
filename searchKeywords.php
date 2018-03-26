2bb56a167e8829700e5fa0316a9cc97a6d6b6a45
lucycopp <lucyanncopp@hotmail.co.uk>
2018-03-26 12:12:52 +0100

    Added functionality to search rooms

---
+++ searchKeywords.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucy
+ * Date: 26/03/2018
+ * Time: 12:12
+ */
+
+require 'index.php';
+$id = $_GET['ID'];
+
+
+
+$sql = "SELECT * FROM Keywords WHERE \"Location ID\"=(?) FOR JSON AUTO";
+$parameters =  array($id);
+$stmt = sqlsrv_query($conn, $sql, $parameters);
+if ($stmt) {
+    while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_NUMERIC)) {
+        echo $row[0];
+    }
+    sqlsrv_free_stmt($stmt);
+}
+else {die(print_r(sqlsrv_errors(), true)); }
+
+?>
