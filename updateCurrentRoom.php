c3aa30b3fba57c927eb03e17547d39ca065262bd
lucycopp <lucyanncopp@hotmail.co.uk>
2018-04-12 13:28:06 +0100

    Added functionality to update a users current location

--- updateCurrentRoom.php
+++ updateCurrentRoom.php
@@ -14,6 +14,7 @@ $params = array($roomID, $userID);
 
 $stmt = sqlsrv_query( $conn, $sql, $params);
 if( $stmt === false ) {
+    echo (sqlsrv_query( $conn, $sql, $params));
     die( print_r( sqlsrv_errors(), true));
 }else {echo "Updated"; }
 
