88da39de04740a9c341fe8ac35ce17bc81764740
lucycopp <lucyanncopp@hotmail.co.uk>
2018-04-12 13:39:05 +0100

    Added functionality to update a users current location

--- updateCurrentRoom.php
+++ updateCurrentRoom.php
@@ -9,7 +9,8 @@
 $userID = $_GET['userID'];
 $roomID = $_GET['roomID'];
 
-$sql = "UPDATE UserTable SET \"Location ID\" = (?) WHERE \"User ID\" = (?)";
+//$sql = "UPDATE UserTable SET \"Location ID\" = (?) WHERE \"User ID\" = (?)";
+$sql = "SELECT (?) AS \"Location ID\" FROM UserTable WHERE \"User ID\" = (?)";
 $params = array($roomID, $userID);
 
 $stmt = sqlsrv_query( $conn, $sql, $params);
