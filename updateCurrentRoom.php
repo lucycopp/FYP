ad4624b28016bcb388faa71b387d35088652a064
lucycopp <lucyanncopp@hotmail.co.uk>
2018-04-12 13:21:24 +0100

    Added functionality to update a users current location

---
+++ updateCurrentRoom.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lucy
+ * Date: 12/04/2018
+ * Time: 13:16
+ */
+
+$userID = $_GET['userID'];
+$roomID = $_GET['roomID'];
+
+$sql = "UPDATE UserTable SET  (\"Location ID\") VALUE (?) WHERE (\"User ID\") VALUE (?)";
+$params = array($roomID, $userID);
+
+$stmt = sqlsrv_query( $conn, $sql, $params);
+if( $stmt === false ) {
+    die( print_r( sqlsrv_errors(), true));
+}else {echo "Updated"; }
+
+?>
